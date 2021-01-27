e5cb18b5baf163deadff0ff368e929a56da921da
TouwaErioer <duhongyu627@gmail.com>
2021-01-27 14:07:55 +0800

    feat:添加headers

---
+++ src/components/headers.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="simple-header">
+        <span class="el-icon-arrow-left icon" @click="back()"></span>
+        <div class="simple-header-name">
+            <slot/>
+        </div>
+        <span class="el-icon-more icon_n"></span>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "headers",
+        methods: {
+            back() {
+                this.$router.back()
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "src/style/header.scss";
+</style>
