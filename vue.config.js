24eb37629a3e3b4b97cc5854740a183e45b6340a
TouwaErioer <duhongyu627@gmail.com>
2021-02-12 14:26:07 +0800

    feat:配置环境变量

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
         proxy: {
             // detail: https://cli.vuejs.org/config/#devserver-proxy
             "/api": {
-                target: `http://127.0.0.1:8081`,
+                target: process.env.target,
                 changeOrigin: true,
                 pathRewrite: {
                     "^/api": ''
