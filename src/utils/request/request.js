86b4a0ac442d085a90033c5724d320a36e1effbc
TouwaErioer <duhongyu627@gmail.com>
2021-02-17 18:45:21 +0800

    fix:token过期自动跳转login

--- src/utils/request/request.js
+++ src/utils/request/request.js
@@ -4,18 +4,18 @@ import router from '@/router/index'
 
 import qs from "qs"
 
-let loadingInstance = null     // 加载全局的loading
+let loadingInstance = null;     // 加载全局的loading
 
 const instance = axios.create({
     timeout: 10000, // 设置超时时间10s
     baseURL: '/api' //根据配置的反向代理去设置不同环境的baeUrl
-})
+});
 
 // instance.defaults.headers.post['Content-Type'] = 'application/json'
 // 统一设置post请求头
-instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // 统一设置delete请求头
-instance.defaults.headers.delete['Content-Type'] = 'application/x-www-form-urlencoded'
+instance.defaults.headers.delete['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let httpCode = {
     400: '请求参数错误',
@@ -26,28 +26,32 @@ let httpCode = {
     501: '服务器不支持该请求中使用的方法',
     502: '网关错误',
     504: '网关超时'
-}
+};
 
 /** 添加请求拦截器 **/
 instance.interceptors.request.use(config => {
-    config.headers['Authorization'] = localStorage.getItem('token') || ''
+    config.headers['Authorization'] = localStorage.getItem('token') || '';
     loadingInstance = Loading.service({       // 发起请求时加载全局loading，请求失败或有响应时会关闭
         spinner: 'fa fa-spinner fa-spin fa-3x fa-fw',
         text: '拼命加载中...'
-    })
+    });
     return config
 }, error => {
     // 对请求错误做些什么
     return Promise.reject(error)
-})
+});
 
 /** 添加响应拦截器  **/
 instance.interceptors.response.use(response => {
-    loadingInstance.close()
-    // todo code: 1 -> 成功
-    //            0 -> 失败
-    //           -1 -> 系统错误
-    if (response.data.code === 1) {
+    loadingInstance.close();
+    if (response.data.message === 'token过期') {
+        Message({
+                message: 'token过期，请重新登录',
+                type: 'error'
+            });
+        localStorage.removeItem('token');
+        router.replace('/login').then();
+    } else if (response.data.code === 1) {
         return Promise.resolve(response.data)
     } else {
         Message({
@@ -56,29 +60,23 @@ instance.interceptors.response.use(response => {
         })
     }
 }, error => {
-    loadingInstance.close()
+    loadingInstance.close();
     if (error.response) {
         // 根据请求失败的http状态码去给用户相应的提示
-        let tips = error.response.status in httpCode ? httpCode[error.response.status] : error.response.data.message
+        let tips = error.response.status in httpCode ? httpCode[error.response.status] : error.response.data.message;
         Message({
             message: tips,
             type: 'error'
-        })
-
-        if (error.response.status === 401 && localStorage.getItem('token') !== null) {// token或者登陆失效情况下跳转到登录页面，根据实际情况，在这里可以根据不同的响应错误结果，做对应的事。这里我以401判断为例
-            localStorage.removeItem('token');
-            router.push('/login')
-        }
-
+        });
         return Promise.reject(error)
     } else {
         Message({
             message: '请求超时, 请刷新重试',
             type: 'error'
-        })
+        });
         return Promise.reject(new Error('请求超时, 请刷新重试'))
     }
-})
+});
 
 /* 统一封装get请求 */
 export const get = (url, params, config = {}) => {
@@ -94,7 +92,7 @@ export const get = (url, params, config = {}) => {
             reject(error)
         })
     })
-}
+};
 
 /* 统一封装post请求  */
 export const post = (url, data, config = {}) => {
@@ -116,7 +114,7 @@ export const post = (url, data, config = {}) => {
             reject(error)
         })
     })
-}
+};
 
 export const del = (url, data, config = {}) => {
     if (config.headers != null) {
@@ -137,4 +135,4 @@ export const del = (url, data, config = {}) => {
             reject(error)
         })
     })
-}
+};
