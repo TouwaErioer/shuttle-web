6097e6810482936b636f47e5b66c4dbb6ec06a71
TouwaErioer <duhongyu627@gmail.com>
2021-02-25 12:23:02 +0800

    fix:支付状态页

--- src/page/center/result.vue
+++ src/page/center/result.vue
@@ -6,11 +6,8 @@
             <span slot="icon" @click="$router.replace('/home')"><i class="el-icon-house"></i></span>
         </Headers>
         <div slot="center" class="container">
-            <el-button :icon="status > 0?'el-icon-circle-check':'el-icon-circle-close'"
-                       @click="$router.replace('/center/balance')"
-                       :type="status > 0?'success':'danger'" plain>
-                {{status > 0?'成功':'失败'}}
-            </el-button>
+            <Empty :description="status > 0 ?'成功':'失败'"
+                   :svg="status > 0 ?require('@/assets/undraw_successful_purchase_uyin.svg'):require('@/assets/undraw_access_denied_6w73.svg')"/>
         </div>
     </Page>
 
@@ -19,10 +16,11 @@
 <script>
     import Headers from "@/components/headers";
     import Page from "@/layout/page";
+    import Empty from "../../components/empty";
 
     export default {
         name: "result",
-        components: {Page, Headers},
+        components: {Empty, Page, Headers},
         props: ['status']
     }
 </script>
