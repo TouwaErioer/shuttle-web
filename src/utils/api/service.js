7c98a908436d88dd76745359c226d2382fc5e2cf
TouwaErioer <duhongyu627@gmail.com>
2021-03-09 10:23:43 +0800

    fix:service order

--- src/utils/api/service.js
+++ src/utils/api/service.js
@@ -1,6 +1,3 @@
 import {get} from '@/utils/request/request'
 
-// 查询全部的服务
-export const findAllService = () => get('/service/findAll')
-
-// export const DeleteService = (params) => Delete('/service/delete/',params)
+export const findAllService = () => get('/service/findAll?order=ASC');
