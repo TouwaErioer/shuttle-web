2691687c84e6e9a866ba58322e2edbb2dac73648
TouwaErioer <duhongyu627@gmail.com>
2021-04-09 14:26:37 +0800

    fix:移出反代配置

--- vue.config.js
+++ vue.config.js
@@ -59,18 +59,18 @@ module.exports = {
         };
     },
 
-    devServer: {
-        host: '0.0.0.0',
-        proxy: {
-            // detail: https://cli.vuejs.org/config/#devserver-proxy
-            "/api": {
-                target: process.env.target,
-                changeOrigin: true,
-                pathRewrite: {
-                    "^/api": ''
-                }
-            }
-        },
-    }
+    // devServer: {
+    //     host: '0.0.0.0',
+    //     proxy: {
+    //         // detail: https://cli.vuejs.org/config/#devserver-proxy
+    //         "/api": {
+    //             target: process.env.target,
+    //             changeOrigin: true,
+    //             pathRewrite: {
+    //                 "^/api": ''
+    //             }
+    //         }
+    //     },
+    // }
 
 };
