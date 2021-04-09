e1d6d94dc79a83a35194dd2447211d0161c735fc
TouwaErioer <duhongyu627@gmail.com>
2021-04-09 14:00:34 +0800

    feat:修改service页样式

--- src/views/service.vue
+++ src/views/service.vue
@@ -1,45 +1,62 @@
 <template>
     <div>
-        <div class="service" style="background: #6395edc9" @click="$router.push('/order')">
-            <span>
-                <i class="el-icon-tickets"></i> 订单
-            </span>
-        </div>
-        <div class="service" style="background: #ff7f50a8" @click="$router.push('/receive')">
-            <span>
-                <i class="el-icon-sell"></i> 接单
-            </span>
-        </div>
-        <div class="service" style="background: pink" @click="$router.push('/cart')">
-            <span>
-                <i class="el-icon-shopping-cart-1"></i> 购物车
-            </span>
-        </div>
-        <div class="service" style="background: thistle" v-if="admin" @click="openAdminWeb">
-            <span>
-                <i class="el-icon-data-analysis"></i> 后台管理
-            </span>
-        </div>
+        <card v-for="(page,index) in pages" :key="index" :title="page.title" :detail="page.detail" :color="page.color"
+              :icon="page.icon">
+            <div id="btn" :style="'background-color: ' + page.color" slot="btn" @click="page.router === 'admin' ? openAdminWeb() :$router.push(page.router)">进入
+            </div>
+        </card>
     </div>
 </template>
 
 <script>
 
+    import card from "@/components/card";
+
     export default {
         name: "service",
+        components: {card},
         data() {
             return {
-                adminTarget: null,
-                admin: false
+                pages: [
+                    {
+                        title: '订单',
+                        detail: '查看已下单的商品',
+                        color: 'rgba(99, 149, 237, 0.79)',
+                        icon: 'el-icon-tickets',
+                        router: '/order'
+                    },
+                    {
+                        title: '接单',
+                        detail: '查看已接收的订单',
+                        color: '#ff7f50a8',
+                        icon: 'el-icon-sell',
+                        router: '/receive'
+                    },
+                    {
+                        title: '购物车',
+                        detail: '查看购物车',
+                        color: 'pink',
+                        icon: 'el-icon-shopping-cart-1',
+                        router: '/cart'
+                    },
+                ]
             }
         },
         created() {
-            this.adminTarget = process.env.VUE_APP_ADMIN;
-            this.admin = this.$store.getters.getUserInfo.admin;
+            let admin = this.$store.getters.getUserInfo.admin;
+            if (admin) {
+                this.pages.push({
+                    title: '后台管理',
+                    detail: '进入后台管理界面',
+                    color: 'thistle',
+                    icon: 'el-icon-data-analysis',
+                    router: 'admin'
+                })
+            }
         },
         methods: {
             openAdminWeb() {
-                window.open(this.adminTarget);
+                window.open(process.env.VUE_APP_ADMIN);
             }
         }
     }
@@ -54,5 +71,15 @@
         align-items: center;
         color: white;
         border-radius: 10px;
+    }
+
+    #btn{
+        width: 50px;
+        height: 30px;
+        font-size: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
     }
 </style>
