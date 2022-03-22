2d3a4c6f360b76f21afd10c8b5b0c089a61398b2
duhongyu <duhongyu@szlx.co>
2022-03-22 11:32:05 +0800

    fix:修复 bug

--- src/utils/api/service.js
+++ src/utils/api/service.js
@@ -1,3 +1,5 @@
 import {get} from '@/utils/request/request'
 
 export const findAllService = () => get('/major/service/findAll?order=ASC');
+
+export const businessList = () => get('/major/service/list');
