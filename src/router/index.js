7a94cbd02ccc85863047f0764c7ad3fc188af817
duhongyu <duhongyu@szlx.co>
2022-04-13 17:33:17 +0800

    feat(router): 动态 title

--- src/router/index.js
+++ src/router/index.js
@@ -16,66 +16,111 @@ const routes = [
     },
     {
         path: '/login',
-        component: () => import('@/page/login')
+        component: () => import('@/page/login'),
+        meta: {
+            title: '登录'
+        }
     },
     {
         path: '/home',
-        component: () => import('@/layout/HomeLayout')
+        component: () => import('@/layout/HomeLayout'),
+        meta: {
+            title: '首页'
+        }
     },
     {
         path: '/service/:sid',
         component: () => import('@/page/explore/stores'),
-        props: true
+        props: true,
+        meta: {
+            title: '商店列表'
+        }
     },
     {
         path: '/store/:sid',
         component: () => import('@/page/explore/store'),
-        props: true
+        props: true,
+        meta: {
+            title: '商店详情'
+        }
     },
     {
         path: '/cart',
-        component: () => import('@/page/explore/cart')
+        component: () => import('@/page/explore/cart'),
+        meta: {
+            title: '购物车'
+        }
     },
     {
         path: '/order',
-        component: () => import('@/page/service/order')
+        component: () => import('@/page/service/order'),
+        meta: {
+            title: '订单'
+        }
     },
     {
         path: '/receive',
-        component: () => import('@/page/service/receive')
+        component: () => import('@/page/service/receive'),
+        meta: {
+            title: '接单'
+        }
     },
     {
         path: '/center/edit',
         component: () => import('@/page/center/edit.vue'),
+        meta: {
+            title: '个人信息'
+        }
     },
     {
         path: '/center/setting',
         component: () => import('@/page/center/setting.vue'),
+        meta: {
+            title: '设置'
+        }
     },
     {
         path: '/center/balance',
         component: () => import('@/page/center/balance.vue'),
+        meta: {
+            title: '充值'
+        }
     },
     {
         path: '/result/:status',
         component: () => import('@/page/center/result'),
-        props: true
+        props: true,
+        meta: {
+            title: '充值结果'
+        }
     },
     {
         path: '/detail',
-        component: () => import('@/page/service/detail')
+        component: () => import('@/page/service/detail'),
+        meta: {
+            title: '订单详情'
+        }
     },
     {
         path: '/star',
-        component: () => import('@/page/service/star')
+        component: () => import('@/page/service/star'),
+        meta: {
+            title: '收藏'
+        }
     },
     {
         path: '/approve',
-        component: () => import('@/page/service/approve')
+        component: () => import('@/page/service/approve'),
+        meta: {
+            title: '审批'
+        }
     },
     {
         path: '/chat',
-        component: () => import('@/page/center/chat')
+        component: () => import('@/page/center/chat'),
+        meta: {
+            title: '客服'
+        }
     }
 ];
 
@@ -85,6 +130,9 @@ const routers = new VueRouter({
 });
 
 routers.beforeEach((to, from, next) => {
+    if (to.meta.title) {
+        document.title = to.meta.title;
+    }
     const token = localStorage.getItem('token');
     let path = to.path;
     let userInfo = common.getUserInfo();
